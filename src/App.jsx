577f8010f5a7a7fca487e411d416f1b3ee54df7f
justinmmorgan21 <justinmorgan21@gmail.com>
2024-10-20 16:14:07 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { QuotesPage } from "./QuotesPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <QuotesPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
